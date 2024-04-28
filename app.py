bc6f47a620660a2fb88358aa7a4d554a0f9cd660
Raaaaming <114052518+raaaaming@users.noreply.github.com>
2024-04-28 13:03:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ async def on_ready():
     print("------")
 
 
-@bot.slash_command()
+@bot.slash_command(description="test")
 async def add(ctx: commands.Context, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(str(left + right))
