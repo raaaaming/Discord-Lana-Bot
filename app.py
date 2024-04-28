f48e7932b627416d28a65c056abc2641b0b4d97d
Raaaaming <114052518+raaaaming@users.noreply.github.com>
2024-04-28 12:54:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,58 +5,90 @@ import os
 
 from keep_alive import keep_alive
 
+import random
+
+import discord
 from discord.ext import commands
 
-import pycord.wavelink as wavelink
+description = """
+An example bot to showcase the discord.ext.commands extension module.
+There are a number of utility commands being showcased here.
+"""
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or("!"),
+    description=description,
+    intents=intents,
+)
 
 
-class Bot(commands.Bot):
-    def __init__(self):
-        super().__init__(command_prefix=">?")
-
-    async def on_ready(self):
-        print("Bot is ready!")
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
 
 
-class Music(commands.Cog):
-    """Music cog to hold Wavelink related commands and listeners."""
+@bot.command()
+async def add(ctx: commands.Context, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(str(left + right))
 
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
 
-        bot.loop.create_task(self.connect_nodes())
+@bot.command()
+async def roll(ctx: commands.Context, dice: str):
+    """Rolls a die in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split("d"))
+    except ValueError:
+        await ctx.send("Format has to be in NdN!")
+        return
 
-    async def connect_nodes(self):
-        """Connect to our Lavalink nodes."""
-        await self.bot.wait_until_ready()
+    # _ is used in the generation of our result as we don't need the number that comes from the usage of range(rolls).
+    result = ", ".join(str(random.randint(1, limit)) for _ in range(rolls))
+    await ctx.send(result)
 
-        await wavelink.NodePool.create_node(
-            bot=self.bot, host="0.0.0.0", port=2333, password="YOUR_LAVALINK_PASSWORD"
-        )
 
-    @commands.Cog.listener()
-    async def on_wavelink_node_ready(self, node: wavelink.Node):
-        """Event fired when a node has finished connecting."""
-        print(f"Node: <{node.identifier}> is ready!")
+@bot.command(description="For when you wanna settle the score some other way")
+async def choose(ctx: commands.Context, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
 
-    @commands.command()
-    async def play(self, ctx: commands.Context, *, search: wavelink.YouTubeTrack):
-        """Play a song with the given search query.
 
-        If not connected, connect to our voice channel.
-        """
-        if not ctx.voice_client:
-            vc: wavelink.Player = await ctx.author.voice.channel.connect(
-                cls=wavelink.Player
-            )
-        else:
-            vc: wavelink.Player = ctx.voice_client
+@bot.command()
+async def repeat(ctx: commands.Context, times: int, *, content: str = "repeating..."):
+    """Repeats a message multiple times."""
+    for _ in range(times):
+        await ctx.send(content)
 
-        await vc.play(search)
+
+@bot.command()
+async def joined(ctx: commands.Context, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f"{member.name} joined in {member.joined_at}")
+
+
+@bot.group()
+async def cool(ctx: commands.Context):
+    """
+    Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f"No, {ctx.subcommand_passed} is not cool")
+
+
+@cool.command(name="bot")
+async def _bot(ctx: commands.Context):
+    """Is the bot cool?"""
+    await ctx.send("Yes, the bot is cool.")
 
 keep_alive()
 
-bot = Bot()
-bot.add_cog(Music(bot))
 token = os.environ.get("TOKEN") or ""
 bot.run(token)
