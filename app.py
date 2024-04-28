c5625b7084f094c9667dffe54834685e868a5090
Raaaaming <114052518+raaaaming@users.noreply.github.com>
2024-04-28 13:38:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ from discord.commands import Option
 
 bot = discord.Bot()
 
-@bot.slash_command(description="라나와 인사하기")
-async def 안녕(ctx):
+@bot.slash_command(name="안녕", description="라나와 인사하기")
+async def hello(ctx):
     await ctx.respond("안녕하세요!")
 
-@bot.slash_command(description="라나의 주사위 굴리기")
-async def 주사위(ctx, text: Option(int, "원하시는 숫자를 적어주세요!")):
+@bot.slash_command(name="주사위", description="라나의 주사위 굴리기")
+async def roll(ctx, text: Option(int, "원하시는 숫자를 적어주세요!")):
     num = random.randrange(1, text + 1)
     await ctx.respond(f":game_die: *{num}!*")
             
