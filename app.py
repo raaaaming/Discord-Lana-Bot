08b3b64cff09402227d501e8697435f65301abf9
Raaaaming <114052518+raaaaming@users.noreply.github.com>
2024-04-28 13:17:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@
 
 import os
 
-from keep_alive import keep_alive
-
 import random
 
 import discord
@@ -14,8 +12,6 @@ bot = discord.Bot()
 @bot.slash_command(description="Hello World 출력하기") # 슬래시 커맨드 등록
 async def helloworld(ctx): # 슬래시 커맨드 이름
     await ctx.respond("Hello World!") # 인터렉션 응답
-    
-keep_alive()
 
 token = os.environ.get("TOKEN") or ""
 bot.run(token)
