7573a95bff63df0d90b9395fdd7c12e62684ab9e
Raaaaming <114052518+raaaaming@users.noreply.github.com>
2024-04-28 13:06:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,86 +8,14 @@ from keep_alive import keep_alive
 import random
 
 import discord
-from discord.ext import commands
 
-description = """
-An example bot to showcase the discord.ext.commands extension module.
-There are a number of utility commands being showcased here.
-"""
+bot = discord.Bot()
+token = "bot_token" # 실제 봇 토큰으로 변경
 
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-
-bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or("!"),
-    description=description,
-    intents=intents,
-)
-
-
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
-    print("------")
-
-
-@bot.slash_command(description="test")
-async def add(ctx: commands.Context, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(str(left + right))
-
-
-@bot.command()
-async def roll(ctx: commands.Context, dice: str):
-    """Rolls a die in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split("d"))
-    except ValueError:
-        await ctx.send("Format has to be in NdN!")
-        return
-
-    # _ is used in the generation of our result as we don't need the number that comes from the usage of range(rolls).
-    result = ", ".join(str(random.randint(1, limit)) for _ in range(rolls))
-    await ctx.send(result)
-
-
-@bot.command(description="For when you wanna settle the score some other way")
-async def choose(ctx: commands.Context, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-
-@bot.command()
-async def repeat(ctx: commands.Context, times: int, *, content: str = "repeating..."):
-    """Repeats a message multiple times."""
-    for _ in range(times):
-        await ctx.send(content)
-
-
-@bot.command()
-async def joined(ctx: commands.Context, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send(f"{member.name} joined in {member.joined_at}")
-
-
-@bot.group()
-async def cool(ctx: commands.Context):
-    """
-    Says if a user is cool.
-
-    In reality this just checks if a subcommand is being invoked.
-    """
-
-    if ctx.invoked_subcommand is None:
-        await ctx.send(f"No, {ctx.subcommand_passed} is not cool")
-
-
-@cool.command(name="bot")
-async def _bot(ctx: commands.Context):
-    """Is the bot cool?"""
-    await ctx.send("Yes, the bot is cool.")
-
+@bot.slash_command(description="Hello World 출력하기") # 슬래시 커맨드 등록
+async def helloworld(ctx): # 슬래시 커맨드 이름
+    await ctx.respond("Hello World!") # 인터렉션 응답
+    
 keep_alive()
 
 token = os.environ.get("TOKEN") or ""
