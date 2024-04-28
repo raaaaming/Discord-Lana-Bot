07c329f8ac9ab49cded01d34471300429c023282
Raaaaming <114052518+raaaaming@users.noreply.github.com>
2024-04-28 13:32:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,20 @@ import os
 import random
 
 import discord
+from discord.commands import Option
 
 bot = discord.Bot()
 
-@bot.slash_command(description="Hello World 출력하기") # 슬래시 커맨드 등록
-async def helloworld(ctx): # 슬래시 커맨드 이름
-    await ctx.respond("Hello World!") # 인터렉션 응답
+@bot.slash_command(description="라나와 인사하기")
+async def 안녕(ctx):
+    await ctx.respond("안녕하세요!")
+
+@bot.slash_command(description="라나의 주사위 굴리기")
+async def 주사위(ctx, text: Option(int, "원하시는 숫자를 적어주세요!")):
+    num = random.randrange(1, text + 1)
+    await ctx.respond(f":game_die: *{num}!*")
+            
+    
 
 token = os.environ.get("TOKEN") or ""
 bot.run(token)
